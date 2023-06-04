686191170412ae814f99eea7de52c00d606f49b2
Palanikumar G <palanikumar.cs@gmail.com>
2023-06-04 19:35:19 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import UserTable from "./component/Usertable";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import Usertable from "./component/Usertable";
+import { Row } from "react-bootstrap";
 
 function App() {
   const dispatch = useDispatch();
   const { users, loading } = useSelector((state) => state.user);
   const { isLoading, setIsLoading } = useState(false);
-
+  const firstObject = Object.keys(users);
   useEffect(() => {
     dispatch(getUsers());
   }, []);
+
+  const getKey = (e) => {
+    alert(e.target.getAttribute("data-item"));
+  };
 
   if (loading) return "...Loading...";
 
@@ -23,9 +28,35 @@ function App() {
       <div className="App">
         <h1>Using Redux toolkit to fetch data from API</h1>
         <p>Please select checkbox to enble each table key value</p>
-        {users.map((items, index) => {
-          return <Usertable items={items} index={index} />;
-        })}
+        <Table responsive striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th> Key </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((items, index) => {
+              return (
+                index === 0 &&
+                Object.keys(items).map((key) => {
+                  return (
+                    <tr key={index + key}>
+                      <td
+                        data-item={key}
+                        onClick={(e) => getKey(e)}
+                        key={key + items.id}
+                      >
+                        {key}
+                      </td>
+                    </tr>
+                  );
+                })
+              );
+            })}
+
+            {/* <Usertable items={items} index={index} />;    */}
+          </tbody>
+        </Table>
       </div>
     </Container>
   );
---
+++ src/component/Rowrender.js
@@ -0,0 +1,17 @@
+import React,{useState} from "react"
+import Table from 'react-bootstrap/Table'
+import Form from 'react-bootstrap/Form';
+
+
+const Rowrender =()=>{
+    
+    return()
+    
+    
+    
+    
+    
+    
+}
+
+export default Rowrender;
