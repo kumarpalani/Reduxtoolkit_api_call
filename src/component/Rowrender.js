0be40e809d30f9ea87f8683499e706ce0ce4b62a
Palanikumar G <palanikumar.cs@gmail.com>
2023-06-06 14:33:31 -0700

    form submit action to display row data

--- src/component/Rowrender.js
+++ src/component/Rowrender.js
@@ -1,36 +1,98 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
 import Columnrender from "./Columnrender";
 
+
 const Rowrender = ({ users }) => {
   const [rowKey, setRowkey] = useState([]);
-  const getKey = (e) => {
-    const rowKeyval = e.target.getAttribute("data-item");
-    if (rowKey.includes(rowKeyval)) {
-      return true;
+
+  
+  const checkboxRef = useRef(null)
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedFilters, setSelectedFilers] = useState([]);
+
+
+  const handleChange =(event,position,checkTxt)=>{
+
+    const {checked} = event.target;
+
+    // if (checked) {
+    //   console.log(typeof(checkTxt));
+    //   setSelectedFilers((prev) => [...prev, checkTxt]);
+    // } else {
+    //   setSelectedFilers(selectedFilters.filter((x) => x !== checkTxt));
+    // }
+
+
+  }
+
+//New code for filter key 
+  const handleSubmit= event=>{
+ 
+     event.preventDefault();
+
+   
+const inputs = Array.from(event.target);
+const valuechange = []
+  inputs.forEach(input => {
+ 
+    if (input.checked) {
+      if(selectedFilters.includes(input.value)) return true
+      valuechange.push(input.value)
+
+    }else{
+      if(selectedFilters.includes(input.value)) {
+        setSelectedFilers(selectedFilters.filter((x) => x !==input.value));
+      
+}
     }
-    setRowkey([...rowKey, rowKeyval]);
-  };
+
+
+  });
+    
+  setSelectedFilers((prev) => [...prev, ...valuechange]);
+
+  }
+  
+
+  ////onclick on key row
+
+  // const getKey = (e) => {
+  //   const rowKeyval = e.target.getAttribute("data-item");
+  //   if (rowKey.includes(rowKeyval)) {
+  //     return true;
+  //   }
+  //   setRowkey([...rowKey, rowKeyval]);
+  // };
+  
   return (
     <>
+
+<form onSubmit={(event)=>{handleSubmit(event)}}>
       <Table responsive striped bordered hover variant="dark">
         <thead>
+     
           <tr>
-            <th> Key </th>
+        
           </tr>
         </thead>
         <tbody>
           {users.map((items, index) => {
             return (
               index === 0 &&
-              Object.keys(items).map((key) => {
+              Object.keys(items).map((key,j) => {
                 return (
+
+                  
                   <tr key={index + key}>
+                    <td>
+                    
+                   <Form.Check ref={checkboxRef} defaultChecked={false} onChange={(event)=>{handleChange(event,j,key)}} aria-label="option 1"  value={key} /> </td>
                     <td
-                      style={{ cursor: "pointer" }}
+                      // style={{ cursor: "pointer" }}
                       data-item={key}
-                      onClick={(e) => getKey(e)}
+                      // onClick={(e) => getKey(e)}
                       key={key + items.id}
                     >
                       {key}
@@ -42,7 +104,14 @@ const Rowrender = ({ users }) => {
           })}
         </tbody>
       </Table>
-      <Columnrender items={users} userKey={rowKey} />
+      
+
+
+<input type="submit" value="Submit" />
+        </form>
+
+    
+        <Columnrender items={users}  userKey={selectedFilters} />
     </>
   );
 };
